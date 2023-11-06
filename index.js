f4de33db90ea1e66318f4dd3fc994f7927b98ddc
sarkar niloy <sarkarniloy102@gmail.com>
2023-11-07 03:37:06 +0600

    connect with mongodb & get popularServices data from database

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.db_USER}:${process.env.db_PASS}@cluster0.mypgnvz.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(process.env.db_USER);
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const popularservicecollection = client.db('Career_Maker').collection('popular_services');
+
+    // popular services related api
+    app.get('/popularServices', async (req, res) => {
+        const cursor = popularservicecollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+
+    })
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 // GET method route
